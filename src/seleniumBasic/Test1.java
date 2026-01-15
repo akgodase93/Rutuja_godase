04ae33f41b4ba634d6ee742e67faaacf4a2e319d
AK93 <akshaykumargodase@gmail.com>
2026-01-15 20:48:16 +0530

    added for  loopin test class

--- src/seleniumBasic/Test1.java
+++ src/seleniumBasic/Test1.java
@@ -5,6 +5,11 @@ public class Test1 {
 	public static void main(String[] args) 
 	{
 		System.out.println("This is test class, to practice git ");
+		
+		for(int i=0; i<=10;i++)
+		{
+			System.out.println(i);
+		}
 	}
 
 }
