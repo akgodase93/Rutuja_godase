75f80e426da15f1328d0950bf15c7e4cfc10a683
AK93 <akshaykumargodase@gmail.com>
2026-01-15 20:36:07 +0530

    added new line in existing code

--- src/seleniumBasic/Test1.java
+++ src/seleniumBasic/Test1.java
@@ -5,6 +5,7 @@ public class Test1 {
 	public static void main(String[] args) 
 	{
 		System.out.println("This is test class, to practice git ");
+		System.out.println("adding new line to merge PR using command line");
 	}
 
 }
