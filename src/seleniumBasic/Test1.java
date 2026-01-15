f7a7854cc60e133d3591a6dc1d3fa105aa2a6203
AK93 <akshaykumargodase@gmail.com>
2026-01-15 21:12:42 +0530

    main commit

--- src/seleniumBasic/Test1.java
+++ src/seleniumBasic/Test1.java
@@ -5,15 +5,11 @@ public class Test1 {
 	public static void main(String[] args) 
 	{
 		System.out.println("This is test class, to practice git ");
-<<<<<<< HEAD
-		System.out.println("adding new line to merge PR using command line");
-=======
 		
 		for(int i=0; i<=10;i++)
 		{
 			System.out.println(i);
 		}
->>>>>>> JAN26
 	}
 
 }
